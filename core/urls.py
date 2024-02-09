76bc5e20d03f047ad3c5e7830629ae6feba3a3bd
fibox24 <fibox.adm@gmail.com>
2024-02-09 09:55:23 -0300

    Cambios necesarios para que la app funciones

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from .views import *
 
 urlpatterns = [
-    path('Inicio',Inicio, name='Inicio'),
+    path('',Inicio, name='Inicio'),
     path('Planes',Planes, name='Planes'),
     path('Quienes_somos',Quienes_somos, name='Quienes_somos'),
     path('Contacto', include('contacto.urls'), name='Contacto'),
